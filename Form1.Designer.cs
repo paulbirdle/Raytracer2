1ae9234bc201b7164b1bdda709dbd10754fe6c03
YoriGit <notnalotte@gmail.com>
2022-03-02 18:44:43 +0100

    Metalicscene

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -34,24 +34,25 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(429, 38);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox1.Location = new System.Drawing.Point(483, 48);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(1440, 886);
+            this.pictureBox1.Size = new System.Drawing.Size(1620, 1108);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(53, 38);
-            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Location = new System.Drawing.Point(60, 48);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(277, 130);
+            this.button1.Size = new System.Drawing.Size(312, 162);
             this.button1.TabIndex = 1;
             this.button1.Text = "Start Render";
             this.button1.UseVisualStyleBackColor = true;
@@ -60,44 +61,55 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(160, 194);
+            this.label1.Location = new System.Drawing.Point(180, 242);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(71, 17);
+            this.label1.Size = new System.Drawing.Size(79, 20);
             this.label1.TabIndex = 2;
             this.label1.Text = "0,00000 s";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(160, 233);
+            this.label2.Location = new System.Drawing.Point(180, 291);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(71, 17);
+            this.label2.Size = new System.Drawing.Size(79, 20);
             this.label2.TabIndex = 3;
             this.label2.Text = "0,00000 s";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(89, 194);
+            this.label3.Location = new System.Drawing.Point(100, 242);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(50, 17);
+            this.label3.Size = new System.Drawing.Size(55, 20);
             this.label3.TabIndex = 4;
             this.label3.Text = "render";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(61, 233);
+            this.label4.Location = new System.Drawing.Point(69, 291);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(78, 17);
+            this.label4.Size = new System.Drawing.Size(87, 20);
             this.label4.TabIndex = 5;
             this.label4.Text = "darstellung";
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(67, 376);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(304, 147);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Antialliasing";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1913, 1024);
+            this.ClientSize = new System.Drawing.Size(2152, 1280);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
@@ -122,6 +134,7 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -25,10 +25,12 @@ namespace Raytracer
 
         private void button1_Click(object sender, EventArgs e)
         {
+            //int resX = 7680; int resY = 4320; //8k
             //int resX = 3840; int resY = 2160; //4K
             //int resX = 1920; int resY = 1080; //FHD
             //int resX = 1080; int resY = 720;  //HD
-            int resX = 640; int resY = 360; //360p
+            //int resX = 640; int resY = 360; //360p
+            int resX = 960; int resY = 540; 
 
             int depth = 5;
             Bitmap flag = new Bitmap(resX, resY);
@@ -80,23 +82,24 @@ namespace Raytracer
         
         private Scene scene2(int resX, int resY)
         {
-            Camera theCamera = new Camera(new Vector(-400, -400, 400), new Vector(2, 2, -1.93), new Vector(1,1,2), Math.PI / 6, resX, resY);
+            Camera theCamera = new Camera(new Vector(-400, -400, 400), new Vector(2, 2, -1.93), new Vector(1,1,2), Math.PI / 6.5, resX, resY);
 
             Entity[] theEntities = new Entity[20];
             Lightsource[] theLights = new Lightsource[2];
             //theLights[0] = new ParallelLight(new Vector(0, 1, 1), new RaytracerColor(Color.White));
             //theLights[1] = new PointLight(new Vector(-50,-100, 25), RaytracerColor.White);
-            theLights[1] = new CandleLight(new Vector(0,0,30),new RaytracerColor(Color.Orange));
+            theLights[1] = new CandleLight(new Vector(-40,0,30),60,new RaytracerColor(Color.White));
 
             int b_s = 50; // background_size
-            theEntities[2] = new Quadrilateral(new Vector(b_s, b_s, 0), new Vector(-b_s, b_s, 0),  new Vector(-b_s, -b_s, 0),  new Vector(b_s, -b_s, 0),  Material.TealBG); // "Boden"
-            theEntities[3] = new Quadrilateral(new Vector(b_s, b_s, 0), new Vector(b_s, b_s, b_s), new Vector(b_s,-b_s, b_s),  new Vector(b_s,-b_s, 0), Material.TealBG); // "rechte Wand"
-            theEntities[4] = new Quadrilateral(new Vector(b_s, b_s, 0), new Vector(-b_s, b_s, 0),  new Vector(-b_s, b_s, b_s), new Vector(b_s, b_s, b_s), Material.TealBG); // "linke Wand"
+            Material bgm = new Material(new RaytracerColor(Color.Red),0,50);
+            theEntities[2] = new Quadrilateral(new Vector(b_s, b_s, 0), new Vector(-b_s, b_s, 0),  new Vector(-b_s, -b_s, 0),  new Vector(b_s, -b_s, 0),  bgm); // "Boden"
+            theEntities[3] = new Quadrilateral(new Vector(b_s, b_s, 0), new Vector(b_s, b_s, b_s), new Vector(b_s,-b_s, b_s),  new Vector(b_s,-b_s, 0), bgm); // "rechte Wand"
+            theEntities[4] = new Quadrilateral(new Vector(b_s, b_s, 0), new Vector(-b_s, b_s, 0),  new Vector(-b_s, b_s, b_s), new Vector(b_s, b_s, b_s), bgm); // "linke Wand"
 
             int s_s = 6; // sphere_size natuerlich :)
             int dist = 8;
             int c = 5;
-            Material materialS = new Material(new RaytracerColor(Color.Orange),0.3,1,0,1);
+            Material materialS = new Material(new RaytracerColor(Color.White),1,40);
             for(int i = 0; i < 2; i++)
             {
                 for(int j = 0; j < 2; j++)
@@ -109,6 +112,46 @@ namespace Raytracer
 
             Scene theScene = new Scene(theCamera, theEntities,theLights, RaytracerColor.Black);
             return theScene;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int resX = 7680; int resY = 4320; //8k
+            //int resX = 3840; int resY = 2160; //4K
+            //int resX = 1920; int resY = 1080; //FHD
+            //int resX = 1080; int resY = 720;  //HD
+            //int resX = 640; int resY = 360; //360p
+
+            int depth = 5;
+            Bitmap flag = new Bitmap(resX/2, resY/2);
+            Scene scene = scene2(resX, resY);
+
+            DateTime before = DateTime.Now;
+            RaytracerColor[,] col = scene.render(depth);
+            DateTime after = DateTime.Now;
+
+            TimeSpan duration = after - before;
+            label1.Text = duration.TotalSeconds.ToString() + " s";
+
+            before = DateTime.Now;
+            RaytracerColor avg;
+
+            for (int i = 0; i < resX/2; i++)
+            {
+                for (int j = 0; j < resY/2; j++)
+                {
+                    avg = 0.25 * col[2*i, 2*j] + 0.25 * col[2*i + 1, 2 * j] + 0.25 * col[2 * i, 2 * j + 1] + 0.25 * col[2 * i + 1, 2 * j + 1]; 
+                    flag.SetPixel(i, j, avg.Col);
+                }
+            }
+            pictureBox1.Image = flag;
+
+            flag.Save("scene1.png", ImageFormat.Png);
+            System.Diagnostics.Process.Start("scene1.png");
+            after = DateTime.Now;
+
+            duration = after - before;
+            label2.Text = duration.TotalSeconds.ToString() + " s" + " Rays:" + scene.countout().ToString();
         }
     }
 }
