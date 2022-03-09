7bd94b310d8c3ca79a777edfd891f6c228302b4e
Luis Maier <luis-m-2@outlook.com>
2022-03-09 19:04:58 +0100

    MovingCameraScene bissle weitergemacht

--- VideoScene/MovingCameraScene.cs
+++ VideoScene/MovingCameraScene.cs
@@ -8,12 +8,14 @@ namespace Raytracer
 {
     class MovingCameraScene : VideoScene
     {
+        //Scene verändert sich nicht
         private readonly Entity[] entities;
         private readonly Lightsource[] lights;
         private readonly RaytracerColor ambientColor;
 
         private readonly int n; //Anzahl frames
-        //Camera:
+
+        //Camera verändert sich (bis auf Auflösung):
         private readonly Vector[] position;     //changing in time
         private readonly Vector[] direction;
         private readonly Vector[] up;
@@ -38,10 +40,10 @@ namespace Raytracer
 
         public Scene GetFrame(int k)
         {
-            return new Scene(new Camera(position[k], direction[k], up[k], xangle[k], resx, resy), entities, lights);
+            return new Scene(new Camera(position[k], direction[k], up[k], xangle[k], resx, resy), entities, lights, ambientColor);
         }
 
-        public RaytracerColor[,] renderFrame(int k, int depth)
+        public RaytracerColor[,] RenderFrame(int k, int depth)
         {
             return GetFrame(k).render(depth);
         }
@@ -77,7 +79,125 @@ namespace Raytracer
             return new MovingCameraScene(scene.giveEntities(), scene.giveLights(), scene.giveAmbientColor(), position, direction, up, xangle, resx, resy);
         }
 
-        //public static MovingCameraScene ZoomCam(Scene scene, double zoomFactor, int frames)
+        public static MovingCameraScene ZoomCam(Scene scene, double zoomFactor, int frames) //zoomFactor > 1 : reinzoomen, < 1: rauszoomen
+        {
+            Camera cam = scene.giveCamera();
+            Vector campos = cam.Position;
+            Vector camdir = cam.Direction;
+            Vector camup = cam.Up;
+            int resx = cam.resX;
+            int resy = cam.resY;
+            double xang = cam.xAngle;
+
+            Vector[] position = new Vector[frames];
+            Vector[] direction = new Vector[frames];
+            Vector[] up = new Vector[frames];
+            double[] xangle = new double[frames];
+
+            double SingleZoomFactor = Math.Pow(zoomFactor, 1 / frames);
+
+            for (int i = 0; i < frames; i++)
+            {
+                position[i] = campos;
+                direction[i] = camdir;
+                up[i] = camup;
+
+                xangle[i] = xang;
+                xang /= SingleZoomFactor;
+            }
+
+            return new MovingCameraScene(scene.giveEntities(), scene.giveLights(), scene.giveAmbientColor(), position, direction, up, xangle, resx, resy);
+        }
+
+        public static MovingCameraScene CircularTrackingShot(Scene scene, Vector rotationCenter, Vector axis, double angle, int frames)
+        {
+            Camera cam = scene.giveCamera();
+            Vector campos = cam.Position;
+            Vector camdir = cam.Direction;
+            Vector camup = cam.Up;
+            int resx = cam.resX;
+            int resy = cam.resY;
+            double xang = cam.xAngle;
+
+            Vector[] position = new Vector[frames];
+            Vector[] direction = new Vector[frames];
+            Vector[] up = new Vector[frames];
+            double[] xangle = new double[frames];
+
+            Matrix Rotate = Matrix.Rotation(axis, angle / frames);
+
+            for (int i = 0; i < frames; i++)
+            {
+                xangle[i] = xang;
+
+                position[i] = campos;
+                direction[i] = camdir;
+                up[i] = camup;
+
+                campos = Rotate * (campos - rotationCenter) + rotationCenter;
+                camdir = Rotate * camdir;
+                camup = Rotate * camup;
+            }
+
+            return new MovingCameraScene(scene.giveEntities(), scene.giveLights(), scene.giveAmbientColor(), position, direction, up, xangle, resx, resy);
+        }
+
+        public static MovingCameraScene LinearTrackingShot(Scene scene, Vector movingDirection, double length, int frames)
+        {
+            Camera cam = scene.giveCamera();
+            Vector campos = cam.Position;
+            Vector camdir = cam.Direction;
+            Vector camup = cam.Up;
+            int resx = cam.resX;
+            int resy = cam.resY;
+            double xang = cam.xAngle;
+
+            Vector[] position = new Vector[frames];
+            Vector[] direction = new Vector[frames];
+            Vector[] up = new Vector[frames];
+            double[] xangle = new double[frames];
+
+            Vector translation = (length / frames) * movingDirection;
+
+            for (int i = 0; i < frames; i++)
+            {
+                xangle[i] = xang;
+
+                position[i] = campos;
+                direction[i] = camdir;
+                up[i] = camup;
+
+                campos += translation;
+            }
+
+            return new MovingCameraScene(scene.giveEntities(), scene.giveLights(), scene.giveAmbientColor(), position, direction, up, xangle, resx, resy);
+        }
+
+        public static MovingCameraScene StaticShot(Scene scene, int frames)
+        {
+            Camera cam = scene.giveCamera();
+            Vector campos = cam.Position;
+            Vector camdir = cam.Direction;
+            Vector camup = cam.Up;
+            int resx = cam.resX;
+            int resy = cam.resY;
+            double xang = cam.xAngle;
+
+            Vector[] position = new Vector[frames];
+            Vector[] direction = new Vector[frames];
+            Vector[] up = new Vector[frames];
+            double[] xangle = new double[frames];
+
+            for (int i = 0; i < frames; i++)
+            {
+                xangle[i] = xang;
+                position[i] = campos;
+                direction[i] = camdir;
+                up[i] = camup;
+            }
+
+            return new MovingCameraScene(scene.giveEntities(), scene.giveLights(), scene.giveAmbientColor(), position, direction, up, xangle, resx, resy);
+        }
 
     }
 }
